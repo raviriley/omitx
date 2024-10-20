d837fa5b173753e0432540aeaa5bb9eb0744bd45
Ravi Riley <raviriley@gmail.com>
2024-10-20 04:29:27 -0700

    Add view on explorer and icons

--- app/columns.tsx
+++ app/columns.tsx
@@ -1,17 +1,24 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
-import { MoreHorizontal } from "lucide-react";
+import { Copy, MoreHorizontal, Search } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { toast } from "sonner";
+
+const NETWORK_EXPLORERS = {
+  //   base: "https://basescan.org/tx/",
+  base: "https://sepolia.basescan.org/tx/",
+  polygon: "https://polygonscan.com/tx/",
+  arbitrum: "https://basescan.org/tx/",
+  ethereum: "https://etherscan.io/tx/",
+};
 
 type Transaction = {
   timestamp: Date;
@@ -33,6 +40,10 @@ export const columns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "hash",
     header: "Hash",
+    cell: ({ row }) => {
+      const transaction = row.original;
+      return <div className="truncate">{transaction.hash}</div>;
+    },
   },
   {
     accessorKey: "to",
@@ -67,10 +78,28 @@ export const columns: ColumnDef<Transaction>[] = [
                 toast.success("Copied to clipboard");
               }}
             >
+              <Copy className="h-4 w-4" />
               Copy transaction hash
             </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem>View on explorer</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => {
+                const explorer =
+                  NETWORK_EXPLORERS[
+                    transaction.chain as keyof typeof NETWORK_EXPLORERS
+                  ];
+                if (explorer) {
+                  console.log(`${explorer}${transaction.hash}`);
+                  window.open(`${explorer}${transaction.hash}`, "_blank");
+                } else {
+                  console.error(
+                    `No explorer found for chain: ${transaction.chain}`,
+                  );
+                }
+              }}
+            >
+              <Search className="h-4 w-4" />
+              View on explorer
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       );
