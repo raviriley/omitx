76a0788643d0e853e6f9daade0d3fea29dff2c50
Ravi Riley <raviriley@gmail.com>
2024-10-19 18:29:34 -0700

    Fix typo

--- providers/supabase.provider.ts
+++ providers/supabase.provider.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/database.types";
 
 const supabaseUrl = process.env.SUPABASE_URL!;
-const supabaseKey = process.env.SUPABSE_KEY!;
+const supabaseKey = process.env.SUPABASE_KEY!;
 const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export const SupabaseProvider = {
